a36c130e5608ed1dc256d2793272ddca010914c0
Sandipan Razzaque <s.z.razzaque@gmail.com>
2011-11-14 19:19:41 +0000

    Added book source files, and started ch4

---
+++ ch4/simple_copy.c
@@ -0,0 +1,14 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <tlpi_hdr.h>
+
+#ifndef BUF_SIZ
+#define BUF_SIZ 1024
+#endif
+
+int main(int argc, char *argv[]) {
+
+  printf("Ain't this gangsta?\n");
+
+  exit(EXIT_SUCCESS);
+}
