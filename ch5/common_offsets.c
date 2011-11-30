c2228a6d6fa35b082538494afb956de2e51b2ac4
Sandipan Razzaque <s.z.razzaque@gmail.com>
2011-11-30 21:53:25 +0000

    WIP on ch5

---
+++ ch5/common_offsets.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <tlpi_hdr.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <error_functions.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+  
+  char data[20] = "yo yo wattup?";
+
+  printf("Opening a file\n");
+  int fd = open( argv[1], O_CREAT | O_WRONLY | O_APPEND );
+  if (fd == -1)
+    errExit("open");
+
+  printf("Writing: %s\n", data);
+  int res = write(fd, data, (size_t)20);
+  if (res == -1)
+    errExit("write");
+
+  int os = lseek(fd, 0, SEEK_END);
+  if (os == -1)
+    errExit("lseek");
+
+  printf("Current offset: %ld\n", (long) os);
+
+  printf("Waiting 5 seconds\n");
+  sleep(5);
+
+  os = lseek(fd, 0, SEEK_END);
+  if (os == -1)
+    errExit("lseek");
+
+  printf("Current offset: %ld\n", (long) os);
+
+  printf("Closing file");
+  if (close(fd) == -1)
+    errExit("close");
+
+  exit(EXIT_SUCCESS);
+  
+}
